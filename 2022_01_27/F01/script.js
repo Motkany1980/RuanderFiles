5e82d889470f96a8254fdb78dc8796dca378add3
István Keszericze <50847472+Motkany1980@users.noreply.github.com>
2022-01-27 18:42:11 +0100

    1

--- 2022_01_27/F01/script.js
+++ 2022_01_27/F01/script.js
@@ -8,8 +8,8 @@ function szamol(){
     console.log(" Résztvevők száma ."+szemelyek);
 
     let tejs=tej*szemelyek;
-    let tojass=tojas*szemelyek;
-    let cukors=cukor*szemelyek;
+    let tojass=Math.round(tojas*szemelyek);
+    let cukors=Math.round(cukor*szemelyek);
 
     let osszetevok="<br>Résztvevők száma:"+szemelyek+"<li>"+tejs+" l tej</li>"+tojass+" db tojás <li>"+cukors+"ek kristálycukor </li><br>";
     console.log(osszetevok)
