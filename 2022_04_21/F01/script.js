a7c0dc27f21da78ce6d461d852c57a5bb0e05a5e
Motkany1980 <50847472+Motkany1980@users.noreply.github.com>
2022-04-21 17:41:15 +0200

    a

--- 2022_04_21/F01/script.js
+++ 2022_04_21/F01/script.js
@@ -1,6 +1,27 @@
 "use strict";
 // tsc --watch
-console.log("Start");
+console.log("Start 04.21.");
+// Visszatérési érték és paraméter nélküli eljárás
 function IloveTypeScript() {
     document.write("Gépelek valamit");
 }
+IloveTypeScript();
+// Visszatérési érték nélküli és paraméteres eljárás
+function NegyzetKeruletTeruletEljaras(a) {
+    let kerulet = 4 * a;
+    let terulet = a * a;
+    document.write(`Kerület és terület`);
+    document.write(`<br> - Kerület ${kerulet}`);
+    document.write(`<br> - Terulet ${terulet}`);
+}
+NegyzetKeruletTeruletEljaras(2);
+// Visszatérési értékes és paraméter nélküli függvény
+function RandomGeneraloFuggveny() {
+    return Math.round(Math.random() * 100);
+}
+document.write(`<br> - Random number ${RandomGeneraloFuggveny()}`);
+// Visszatérési értékes és paraméteres függvény
+function NegyzetKeruletFuggveny(a) {
+    return a * 4;
+}
+document.write(`<br> - Kerület ${NegyzetKeruletFuggveny(3)}`);
--- 2022_04_21/F01/script.ts
+++ 2022_04_21/F01/script.ts
@@ -1,6 +1,33 @@
 // tsc --watch
-console.log("Start");
+console.log("Start 04.21.");
 
+// Visszatérési érték és paraméter nélküli eljárás
 function IloveTypeScript():void{
     document.write("Gépelek valamit");
 }
+IloveTypeScript();
+
+// Visszatérési érték nélküli és paraméteres eljárás
+function NegyzetKeruletTeruletEljaras(a:number):void {
+    let kerulet=4*a;
+    let terulet=a*a;
+    document.write(`Kerület és terület`);
+    document.write(`<br> - Kerület ${kerulet}`);
+    document.write(`<br> - Terulet ${terulet}`);
+}
+NegyzetKeruletTeruletEljaras(2);
+
+// Visszatérési értékes és paraméter nélküli függvény
+function RandomGeneraloFuggveny():number {
+  return Math.round(Math.random()*100);
+}
+document.write(`<br> - Random number ${RandomGeneraloFuggveny()}`)
+
+// Visszatérési értékes és paraméteres függvény
+function NegyzetKeruletFuggveny(a:number):number {
+    return a*4;
+}
+document.write(`<br> - Kerület ${NegyzetKeruletFuggveny(3)}`)
+
+
+
